06b0b980531c53259eb6a770e8929e321738ff68
Fuadmd28 <110433657+Fuadmd28@users.noreply.github.com>
2024-08-03 01:32:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -74,7 +74,7 @@ global.welcome = 'https://telegra.ph/file/6922e4375c183c8d1cfcb.jpg' // Welcome 
 global.leave = 'https://telegra.ph/file/8c7792e78ed015a7d0a59.jpg' // Leave Group
 global.vn = 'https://github.com/XM4ZE/DATABASE/raw/master/wallpaper/KARA.mp3?raw=true' // Allmenu Voice
 global.thumvid = 'https://github.com/XM4ZE/DATABASE/raw/master/wallpaper/Vid_20240220_073653.mp4?raw=true' // Allmenu Video thumbnail
-global.maximus = '' // [OPSIONAL] isi dengan link thumbnail kalian. link ini akan muncul di semua menu dan taruh di tengah-tengah simbol itu
+global.maximus = 'https://telegra.ph/file/c936549c76f72b12dcdda.jpg' // [OPSIONAL] isi dengan link thumbnail kalian. link ini akan muncul di semua menu dan taruh di tengah-tengah simbol itu
 
 
 
